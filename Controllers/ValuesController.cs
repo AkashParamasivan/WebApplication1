351953539a8254c4979baaf39c90c50cc1b286a4
Akash.Paramasivan <akash.paramasivan@kanini.com>
2021-04-07 15:05:29 +0530

    Add project files.

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        SpecificationdbContext _DbContext;
+
+        public ValuesController(SpecificationdbContext DbContext)
+        {
+            _DbContext = DbContext;
+        }
+        [HttpGet]
+        public IActionResult GetSpec()
+        {
+            return Ok(_DbContext.SpecificationTables.ToList());
+        }
+    }
+}
---
+++ Models/SpecificationdbContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace WebApplication1.Models
+{
+    public partial class SpecificationdbContext : DbContext
+    {
+        public SpecificationdbContext()
+        {
+        }
+
+        public SpecificationdbContext(DbContextOptions<SpecificationdbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<SpecificationTable> SpecificationTables { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=tcp:kaninidb123.database.windows.net,1433;Initial Catalog=Specificationdb;Persist Security Info=False;User ID=KaniniDemosql;Password=Kanini@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<SpecificationTable>(entity =>
+            {
+                entity.ToTable("SpecificationTable");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
